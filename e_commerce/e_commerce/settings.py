7d6d22fb6d06d922364d4ad22372472c3c298580
SumalRasheed <sumal12rasheed@gmail.com>
2022-11-02 13:46:24 +0530

    heroku commit4

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
